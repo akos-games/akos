eff8be0f1f67fb840c92b60b3599af8dadcbb64d
Sébastien Roux <s.roux@gmx.fr>
2019-08-08 01:37:16 +0200

    #2 game structure UI

--- studio/src/app/structure/structure.component.ts
+++ studio/src/app/structure/structure.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {FlatTreeControl, NestedTreeControl} from "@angular/cdk/tree";
+import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource} from "@angular/material";
+
+interface StructureNode {
+  name: string;
+  icon?: string;
+  addButton?: boolean;
+  removeButton?: boolean;
+  editButton?: boolean;
+  children?: StructureNode[];
+}
 
 @Component({
   selector: 'app-structure',
@@ -7,9 +18,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructureComponent implements OnInit {
 
-  constructor() { }
+  private structure: StructureNode[] = [{
+    name: 'Game metadata',
+    icon: 'list_alt'
+  }, {
+    name: 'Scenes',
+    addButton: true,
+    children: [{
+      name: 'Scene 1',
+      icon: 'movie_creation',
+      editButton: true,
+      removeButton: true
+    }, {
+      name: 'Scene 2',
+      icon: 'movie_creation',
+      editButton: true,
+      removeButton: true
+    }]
+  }];
+
+  private treeControl: NestedTreeControl<StructureNode> = new NestedTreeControl<StructureNode>(
+    node => node.children
+  );
+
+  private dataSource: MatTreeNestedDataSource<StructureNode> = new MatTreeNestedDataSource<StructureNode>();
+
+  constructor() {
+    this.dataSource.data = this.structure;
+  }
 
   ngOnInit() {
   }
 
+  isCategory(index: number, node: StructureNode): boolean {
+    return !!node.children;
+  }
 }
