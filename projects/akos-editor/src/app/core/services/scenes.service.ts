39ef0d93c1a01fd9469ff63863642582504b04d5
Sébastien Roux <s.roux@gmx.fr>
2020-04-15 21:40:48 +0200

    fix removing game first scene

--- projects/akos-editor/src/app/core/services/scenes.service.ts
+++ projects/akos-editor/src/app/core/services/scenes.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { generateId } from '../../shared/utils/entity.util';
 import { Scene } from 'akos-common';
 import { ScenesState } from '../states/scenes.state';
+import { GameState } from '../states/game.state';
 
 @Injectable()
 export class ScenesService {
 
-  constructor(private scenesState: ScenesState) {
+  constructor(
+    private scenesState: ScenesState,
+    private gameState: GameState
+  ) {
   }
 
   createScene(): Scene {
@@ -26,6 +30,13 @@ export class ScenesService {
   }
 
   deleteScene(id: number) {
+
+    let game = this.gameState.get();
+    if (game.firstSceneId === id) {
+      game.firstSceneId = null;
+      this.gameState.set(game);
+    }
+
     this.scenesState.remove(id);
   }
 
