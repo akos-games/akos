3a18a9820c1c6fbb53db0e398d4b327bdac01993
Sébastien Roux <s.roux@gmx.fr>
2020-08-24 23:52:22 +0200

    #84 Fix akos-common build

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,6 +4,7 @@ function build_electron() {
   local project
   project="$1"
 
+  ng build akos-common
   ng build "$project" --prod --base-href ./
   tsc -p projects/"$project"/tsconfig.electron.json
   electron-builder -c projects/"$project"/electron-builder.yml
