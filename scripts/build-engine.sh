ac4969e5ce66fadb2bfa6270d08c5c49f8821bee
Sébastien Roux <s.roux@gmx.fr>
2020-12-11 22:37:01 +0100

    #162 Fix build for Mac

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -4,5 +4,8 @@ source scripts/common.sh
 
 rm -rf dist/build
 build_electron "akos-engine"
-DIR=$(ls -d dist/build/akos-engine/*-unpacked)
-mv "$DIR" "${DIR/-unpacked/}"
+
+if [ ! -d dist/build/akos-engine/mac ]; then
+  DIR=$(ls -d dist/build/akos-engine/*-unpacked)
+  mv "$DIR" "${DIR/-unpacked/}"
+fi
